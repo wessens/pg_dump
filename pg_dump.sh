8ffee26f39e43fbf355186f7357c176297ca8a93
wessens <arseniy.gurin@rowsquared.com>
2023-06-29 09:20:13 +0300

    Update pg_dump.sh

--- pg_dump.sh
+++ pg_dump.sh
@@ -6,9 +6,14 @@
 # PGUSER
 # PGPASSWORD
 
-fname=$(date "+%Y%m%dT%H%M%S")
+date=$(date "+%Y%m%dT%H%M%S")
+fname_dump="${date}.pgdump"
 fname_log="${fname}.log"
-pg_dump -v -w -Fc -f /app/share/backup/$fname --compress=4 --host="$PGHOST" --username="$PGUSER" --dbname="$PGDATABASE" 2> /app/share/log/$fname_log
-sh clean.sh /app/share/backup/
-sh clean.sh /app/share/log/
+pg_dump -v -w -Fc -f /app/share/backup/$fname_dump --compress=4 --host="$PGHOST" --username="$PGUSER" --dbname="$PGDATABASE" 2> /app/share/log/$fname_log
+
+code=$?
+if [ $code -ne 0 ]; then
+  echo 1>&2 "The backup failed (exit code $code), check for errors in $fname_log"
+fi
+
 exit
