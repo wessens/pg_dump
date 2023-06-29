41b0ffa23f05380a13659158c53d1711f6127521
wessens <arseniy.gurin@rowsquared.com>
2023-06-29 18:37:38 +0300

    Update pull_webhook.sh

--- pull_webhook.sh
+++ pull_webhook.sh
@@ -8,6 +8,6 @@ content_type="Content-Type: application/json"
 msg=$1
 data="{\"to\": \"${API_DEST}\", \"subject\": \"${API_SUBJ}\", \"body\": \"$msg\"}"
 
-curl -X POST -H ${auth_token} -H ${content_type} -d "${data}" ${url}
+curl -X POST -H "${auth_token}" -H "${content_type}" -d "${data}" ${url}
 
 echo "Ending webhook pull"
